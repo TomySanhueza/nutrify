8f8db50385f69f8e01a71fc1820b77b348d5726a
TOMÁS IGNACIO. SANHUEZA <tomas.sanhuezaf@gmail.com>
2025-09-24 00:34:30 +0200

    New and create patients controller completed

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,25 @@
 class PatientsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    @patient.user = current_user
+
+    if @patient.save
+      redirect_to @patient, notice: 'Patient created successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :birthday, :weight, :height, :nutrition_plan, :clinical_history)
+  end
+
 end
