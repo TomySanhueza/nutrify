3691072e2884bd2cf7f4b37f742f2e14394d9111
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jeremyarcafire@gmail.com>
2025-09-24 14:47:55 -0500

    Comenta que te parece

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,43 @@
+class ChatsController < ApplicationController
+  before_action :set_patient
+  before_action :set_chat, only: [:show,:destroy]
+
+  def index
+    @chats = @patient.chats
+  end
+
+  def show
+  end
+
+  def new
+    @chat = @patient.chats.new
+  end
+  
+  def create
+    @chat = @patient.chats.new(chat_params)
+    if @chat.save
+      redirect_to patient_chat_path(@patient, @chat), notice: "Chat creado con éxito."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @chat.destroy
+    redirect_to patient_chats_path(@patient), notice: "Chat eliminado con éxito."
+  end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:patient_id])
+  end
+
+  def set_chat
+    @chat = @patient.chats.find(params[:id])
+  end
+
+  def chat_params
+    params.require(:chat).permit(:title, :content)
+  end
+end
